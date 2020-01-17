2f12a364e7eb3ca5c94c653b29b625e41170bc68
Hamza Hsain <hhsain@bewizyu.com>
2020-01-17 12:29:39 +0100

    add Home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OnboardComponent } from './onboard/onboard.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'welcome',
     pathMatch: 'full'
   },
   {
     path: 'home',
     component: HomeComponent,
     data: { title: 'Page d\'accueil' }
+  },
+  {
+    path: 'welcome',
+    component: OnboardComponent,
+    data: { title: 'Bienvenue' }
   }
 ];
 
